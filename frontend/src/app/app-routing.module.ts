145d2760ad64213d375fe6e671dd1a407b26d002
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-10-15 20:58:51 -0300

    organiza numa pasta registro

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ClassComponent } from './pages/class/class.component';
-import { LessonComponent } from './pages/lesson/lesson.component';
-import { StudentComponent } from './pages/student/student.component';
-import { SubjectComponent } from './pages/subject/subject.component';
-import { TeacherComponent } from './pages/teacher/teacher.component';
+import { ClassComponent } from './pages/register/class/class.component';
+import { LessonComponent } from './pages/register/lesson/lesson.component';
+import { StudentComponent } from './pages/register/student/student.component';
+import { SubjectComponent } from './pages/register/subject/subject.component';
+import { TeacherComponent } from './pages/register/teacher/teacher.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
