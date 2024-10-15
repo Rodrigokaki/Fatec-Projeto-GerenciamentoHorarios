3e66082fe70e9db7d661f5d91ef1f03012a5c503
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-10-15 14:50:56 -0300

    estrutura inicial angular e navbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ClassComponent } from './pages/class/class.component';
+import { LessonComponent } from './pages/lesson/lesson.component';
+import { StudentComponent } from './pages/student/student.component';
+import { SubjectComponent } from './pages/subject/subject.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'class', component: ClassComponent},
+  {path: 'lesson', component: LessonComponent},
+  {path: 'student', component: StudentComponent},
+  {path: 'subject', component: SubjectComponent},
+  {path: 'teacher', component: TeacherComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubjectComponent } from './pages/subject/subject.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
+import { LessonComponent } from './pages/lesson/lesson.component';
+import { ClassComponent } from './pages/class/class.component';
+import { StudentComponent } from './pages/student/student.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubjectComponent,
+    TeacherComponent,
+    LessonComponent,
+    ClassComponent,
+    StudentComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideClientHydration()
