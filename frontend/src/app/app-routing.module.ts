02a4dff2841ec6f5d3d292d619edc9842c1b611b
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-10-19 00:00:14 -0300

    Adiciona curso no front

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LessonComponent } from './pages/register/lesson/lesson.component';
 import { StudentComponent } from './pages/register/student/student.component';
 import { SubjectComponent } from './pages/register/subject/subject.component';
 import { TeacherComponent } from './pages/register/teacher/teacher.component';
+import { CourseComponent } from './pages/register/course/course.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path: 'student', component: StudentComponent},
   {path: 'subject', component: SubjectComponent},
   {path: 'teacher', component: TeacherComponent},
+  {path: 'course', component: CourseComponent}
 ];
 
 @NgModule({
