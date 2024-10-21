fdc6c34b0cb38a31113688d4ddfed933c37c0642
leogalencar <leonardogalencar444@gmail.com>
2024-10-20 22:35:21 -0300

    course service

--- frontend/src/app/pages/course/course.component.ts
+++ frontend/src/app/pages/course/course.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ICourse } from '../../models/ICourse';
+import { CourseService } from '../../services/course.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './course.component.css'
 })
 export class CourseComponent {
+  courses: ICourse[] = [];
 
+  constructor(private courseService: CourseService, private _router: Router) {}
+
+  ngOnInit(): void {
+    this.loadCourses();
+  }
+
+  loadCourses(): void {
+    this.courseService.getCourses().subscribe({
+      next: (data) => {
+        this.courses = data;
+      }
+    })
+  }
+
+  updateCourse(course: ICourse): void {
+    this.courseService.setSharedCourse(course);
+    this._router.navigate(['/course/register', {data: {isEditing: true}}]);
+  }
+
+  deleteCourse(course: ICourse): void {
+    this.courseService.deleteCourse(course).subscribe({
+      next: (data) => {
+        this.loadCourses();
+      }
+    })
+  }
 }
