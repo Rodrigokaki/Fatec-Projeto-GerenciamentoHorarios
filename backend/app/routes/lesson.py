ea595c5abd8206222ccdccdb78e2dfd2672c203d
leogalencar <leonardogalencar444@gmail.com>
2024-11-14 11:29:42 -0300

    refactor: lessons endpoint migration to MongoDB

--- backend/app/routes/lesson.py
+++ backend/app/routes/lesson.py
@@ -1,59 +1,88 @@
 from flask import Blueprint, jsonify, request
-from ..models.lesson_model import Lesson
-from app.db import db
+from app.db import mongo
+from ..helpers import jsonify_plain, convert_to_datetime
+from bson import ObjectId
+from bson.errors import InvalidId as bsonInvalidId
+import traceback
 
 bp = Blueprint('lessons', __name__, url_prefix='/lessons')
+PRIMARY_ID_KEY = 'cod_aula'
 
 @bp.route('/', methods=['GET'])
 def get_lessons():
-    lessons = Lesson.query.all()
-    return jsonify([lesson.to_dict() for lesson in lessons])
+    lessons = mongo.db.Aula.find()
+    lessons_list = [jsonify_plain(lesson, id_key=PRIMARY_ID_KEY) for lesson in lessons]
+    
+    return jsonify(lessons_list)
 
-@bp.route('/<int:id>', methods=['GET'])
+@bp.route('/<id>', methods=['GET'])
 def get_lesson_by_id(id):
-    lesson = Lesson.query.get(id)
+    try:
+        lesson = mongo.db.Aula.find_one({"_id": ObjectId(id)})
+    except bsonInvalidId as invalidId:
+        return jsonify({'message': 'ID inválido', 'error': traceback.format_exception_only(invalidId)}), 400
+    
     if lesson:
-        return jsonify(lesson.to_dict())
+            lesson = jsonify_plain(lesson, id_key=PRIMARY_ID_KEY)
+            return jsonify(lesson)
+    
     return jsonify({'message': 'Não encontrado'}), 404
 
 @bp.route('', methods=['POST'])
 def add_lesson():
     data = request.get_json()
     
-    new_lesson = Lesson(
-        horario=data.get('horario'),
-        cod_disciplina=data.get('cod_disciplina'),
-        cod_turma = data.get('cod_turma'),
-        dia_semana = data.get('dia_semana')
-    )
+    try:
+        new_lesson = {
+            'horario': convert_to_datetime(data.get('horario')),
+            'cod_disciplina': ObjectId(data.get('cod_disciplina')),
+            'cod_turma': ObjectId(data.get('cod_turma')),
+            'dia_semana': data.get('dia_semana')
+        }
+    except bsonInvalidId as invalidId:
+        return jsonify({'message': 'ID inválido', 'error': traceback.format_exception_only(invalidId)}), 400
+    except ValueError as ve:
+        return jsonify({'message': 'Os dados estão em um formato inválido', 'error': traceback.format_exception_only(ve)}), 400
 
-    db.session.add(new_lesson)
-    db.session.commit()
+    mongo.db.Aula.insert_one(new_lesson)
 
-    return jsonify(new_lesson.to_dict()), 201
+    return jsonify(jsonify_plain(new_lesson, id_key=PRIMARY_ID_KEY)), 201
 
-@bp.route('/<int:id>', methods=['DELETE'])
+@bp.route('/<id>', methods=['DELETE'])
 def delete_lesson_by_id(id):
-    lesson = Lesson.query.get(id)
-    if lesson is None:
-        return jsonify({"message":"Não encontrado"}), 404
+    try:
+        result = mongo.db.Aula.delete_one({"_id": ObjectId(id)})
+    except bsonInvalidId as invalidId:
+        return jsonify({'message': 'ID inválido', 'error': traceback.format_exception_only(invalidId)}), 400
     
-    db.session.delete(lesson)
-    db.session.commit()
+    if result.deleted_count == 0:
+        return jsonify({"message":"Não encontrado"}), 404
     return jsonify({'message':'Deletado com sucesso!'}), 204
 
-@bp.route('/<int:id>', methods=['PUT'])
+@bp.route('/<id>', methods=['PUT'])
 def update_lesson(id):
-    lesson = Lesson.query.get(id)
-    if lesson is None:
-        return jsonify({"message":"Não encontrado"}), 404
-    
     data = request.get_json()
     
-    lesson.horario = data['horario']
-    lesson.cod_disciplina = data['cod_disciplina']
-    lesson.cod_turma = data['cod_turma']
-    lesson.dia_semana = data['dia_semana']
+    try:
+        updated_lesson = {
+            'horario': convert_to_datetime(data.get('horario')),
+            'cod_disciplina': ObjectId(data.get('cod_disciplina')),
+            'cod_turma': ObjectId(data.get('cod_turma')),
+            'dia_semana': data.get('dia_semana')
+        }
+    except bsonInvalidId as invalidId:
+        return jsonify({'message': 'ID inválido', 'error': traceback.format_exception_only(invalidId)}), 400
+    except ValueError as ve:
+        return jsonify({'message': 'Os dados estão em um formato inválido', 'error': traceback.format_exception_only(ve)}), 400
+    
+    try:
+        result = mongo.db.Aula.update_one({"_id": ObjectId(id)}, {"$set": updated_lesson})
+    except bsonInvalidId as invalidId:
+        return jsonify({'message': 'ID inválido', 'error': traceback.format_exception_only(invalidId)}), 400
 
-    db.session.commit()
-    return jsonify(lesson.to_dict()), 200
+    if result.matched_count == 0:
+        return jsonify({'message': 'Não encontrado'}), 404
+    
+    updated_lesson['cod_aula'] = id
+
+    return jsonify(jsonify_plain(updated_lesson, id_key=PRIMARY_ID_KEY)), 200
