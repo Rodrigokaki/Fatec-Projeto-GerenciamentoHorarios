533defcf4034b67dd3ff3d95b5c9b54761dffb7a
leogalencar <leonardogalencar444@gmail.com>
2024-11-14 11:35:24 -0300

    refactor: courses endpoint migration to MongoDB

--- backend/app/routes/course.py
+++ backend/app/routes/course.py
@@ -1,55 +1,74 @@
 from flask import Blueprint, jsonify, request
-from ..models.course_model import Course
-from app.db import db
+from app.db import mongo
+from ..helpers import jsonify_plain, convert_to_datetime
+from bson import ObjectId
+from bson.errors import InvalidId as bsonInvalidId
+import traceback
 
 bp = Blueprint('courses', __name__, url_prefix='/courses')
+PRIMARY_ID_KEY = 'cod_curso'
 
 @bp.route('/', methods=['GET'])
 def get_courses():
-    courses = Course.query.all()
-    return jsonify([course.to_dict() for course in courses])
+    courses = mongo.db.Curso.find()
+    courses_list = [jsonify_plain(course, id_key=PRIMARY_ID_KEY) for course in courses]
+    
+    return jsonify(courses_list)
 
-@bp.route('/<int:id>', methods=['GET'])
+@bp.route('/<id>', methods=['GET'])
 def get_course_by_id(id):
-    course = Course.query.get(id)
+    try:
+        course = mongo.db.Curso.find_one({"_id": ObjectId(id)})
+    except bsonInvalidId as invalidId:
+        return jsonify({'message': 'ID inválido', 'error': traceback.format_exception_only(invalidId)}), 400
+    
     if course:
-        return jsonify(course.to_dict())
+            course = jsonify_plain(course, id_key=PRIMARY_ID_KEY)
+            return jsonify(course)
+    
     return jsonify({'message': 'Não encontrado'}), 404
 
 @bp.route('', methods=['POST'])
 def add_course():
     data = request.get_json()
     
-    new_course = Course(
-        nome=data.get('nome'),
-        eixo_tecnologico=data.get('eixo_tecnologico')
-    )
+    new_course = {
+        'nome': data.get('nome'),
+        'eixo_tecnologico': data.get('eixo_tecnologico')
+    }
 
-    db.session.add(new_course)
-    db.session.commit()
+    mongo.db.Curso.insert_one(new_course)
 
-    return jsonify(new_course.to_dict()), 201
+    return jsonify(jsonify_plain(new_course, id_key=PRIMARY_ID_KEY)), 201
 
-@bp.route('/<int:id>', methods=['DELETE'])
+@bp.route('/<id>', methods=['DELETE'])
 def delete_course_by_id(id):
-    course = Course.query.get(id)
-    if course is None:
-        return jsonify({"message":"Não encontrado"}), 404
+    try:
+        result = mongo.db.Curso.delete_one({"_id": ObjectId(id)})
+    except bsonInvalidId as invalidId:
+        return jsonify({'message': 'ID inválido', 'error': traceback.format_exception_only(invalidId)}), 400
     
-    db.session.delete(course)
-    db.session.commit()
+    if result.deleted_count == 0:
+        return jsonify({"message":"Não encontrado"}), 404
     return jsonify({'message':'Deletado com sucesso!'}), 204
 
-@bp.route('/<int:id>', methods=['PUT'])
+@bp.route('/<id>', methods=['PUT'])
 def update_course(id):
-    course = Course.query.get(id)
-    if course is None:
-        return jsonify({"message":"Não encontrado"}), 404
-    
     data = request.get_json()
     
-    course.nome = data['nome']
-    course.eixo_tecnologico = data['eixo_tecnologico']
+    updated_course = {
+        'nome': data.get('nome'),
+        'eixo_tecnologico': data.get('eixo_tecnologico')
+    }
+    
+    try:
+        result = mongo.db.Curso.update_one({"_id": ObjectId(id)}, {"$set": updated_course})
+    except bsonInvalidId as invalidId:
+        return jsonify({'message': 'ID inválido', 'error': traceback.format_exception_only(invalidId)}), 400
 
-    db.session.commit()
-    return jsonify(course.to_dict()), 200
+    if result.matched_count == 0:
+        return jsonify({'message': 'Não encontrado'}), 404
+    
+    updated_course['cod_curso'] = id
+
+    return jsonify(jsonify_plain(updated_course, id_key=PRIMARY_ID_KEY)), 200
